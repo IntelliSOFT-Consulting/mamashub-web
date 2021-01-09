f0acab117b6ba6d44932b5826bd6ecf4bd12710b
amolo.me <railamolo@gmail.com>
2021-01-09 17:26:56 +0300

    add routes/sample.ts

--- build/index.js
+++ build/index.js
@@ -4,10 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-// rest of the code remains same
+//Import routes 
+const main_1 = __importDefault(require("./routes/main"));
+const users_1 = __importDefault(require("./routes/users"));
 const app = express_1.default();
-const PORT = 8000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+const PORT = 8080;
+app.use('/', main_1.default);
+app.use('/users', users_1.default);
 app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
