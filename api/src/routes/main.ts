6f58f15bf0a96703291fab241cc33da175a0d169
Amolo <railamolo@gmail.com>
2022-04-18 21:30:41 +0300

    updated swagger docs

--- api/src/routes/main.ts
+++ api/src/routes/main.ts
@@ -1,12 +1,17 @@
 import express from "express";
-import * as SwaggerUI from 'swagger-ui-express'
+import SwaggerUI from 'swagger-ui-express'
+import swaggerJsDoc from 'swagger-jsdoc'
 
-import swaggerDoc from '../swagger.json' 
+import swaggerDoc from '../swagger.json'
 // https://levelup.gitconnected.com/how-to-add-swagger-ui-to-existing-node-js-and-express-js-project-2c8bad9364ce
 
 const router = express.Router()
 
-router.use('/', SwaggerUI.serve)
-router.get('/', SwaggerUI.setup(swaggerDoc))
+router.use(
+    "/",
+    SwaggerUI.serve,
+    SwaggerUI.setup(swaggerDoc,{ explorer: true })
+);
+
 
 export default router
