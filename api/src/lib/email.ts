7324c3a2f1496ddb3ea950bb3b6e2a17a9564d96
Amolo <railamolo@gmail.com>
2022-04-21 14:59:42 +0300

    Updated email template

--- api/src/lib/email.ts
+++ api/src/lib/email.ts
@@ -44,7 +44,7 @@ export let sendPasswordResetEmail = async (user:any, resetLink:string) => {
             body: `Dear ${user.names[0]},\n\n\nUse the link below to reset your password \n\n${resetLink}`,
             html: `<h4>Dear ${user.names[0]},<h4><br/>
             <h5>Use the link below to reset your password<h5/><br/>
-            <a href=${resetLink}><button style="background-color:'#115987'">Reset Link<button></a>
+            <a href=${resetLink}><button style="background-color:#115987;border: none;padding: 15px 32px;">Reset Link<button></a>
             <br/><br/>
             <a href=${"https://human-milk-bank.netlify.app"}>Human Milk Bank Website</a>
             `
