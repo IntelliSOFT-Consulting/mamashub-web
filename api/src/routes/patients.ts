ae1f93c5781e8b278ed476598bb54a12a3c97c31
Amolo <railamolo@gmail.com>
2022-05-10 08:25:12 +0300

    Created Patients Model

--- api/src/routes/patients.ts
+++ api/src/routes/patients.ts
@@ -42,7 +42,50 @@ router.get("/:id",[requireJWT], async (req: Request, res: Response) => {
     }
 });
 
-// Modify User Details
+
+// Create Patient.
+router.post("/", [requireJWT], async (req: Request, res: Response) => {
+    try {
+        let data = req.body;
+        let { id } = req.params
+        let token = req.headers.authorization || '';
+        let decodedSession = decodeSession(process.env['SECRET_KEY'] as string, token.split(' ')[1])
+        if (decodedSession.type == 'valid') {
+            let role = decodedSession.session.role
+            let userId = decodedSession.session.userId
+            if (role !== 'ADMINISTRATOR' || id !== userId) {
+                res.statusCode = 401
+                res.send({ error: `Insufficient Permissions for ${role}`, status: "error" });
+                return
+            }
+        }
+        let user = await db.user.update({
+            where: {
+                id: id
+            },
+            data: data
+        })
+        let responseData = { id: user.id, createdAt: user.createdAt, updatedAt: user.updatedAt, names: user.names, email: user.email, role: user.role }
+        res.statusCode = 201
+        res.json({ user: responseData, status: "success" })
+        return
+    } catch (error: any) {
+        res.statusCode = 400
+        console.error(error)
+        if (error.code === 'P2002') {
+            res.json({ status: "error", message: `User with the ${error.meta.target} provided already exists` });
+            return
+        }
+        res.json(error)
+        return
+    }
+});
+
+
+
+
+
+// Modify Patient Details
 router.post("/:id", [requireJWT], async (req: Request, res: Response) => {
     try {
         let data = req.body;
