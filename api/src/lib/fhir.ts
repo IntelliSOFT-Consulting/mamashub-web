62bdbf462fee5647b45432667eec89fd46cc0fcb
Amolo <railamolo@gmail.com>
2022-05-11 17:58:58 +0300

    fix: typescript typings

--- api/src/lib/fhir.ts
+++ api/src/lib/fhir.ts
@@ -1,3 +1,3 @@
-import {FHIRClient} from "@js-fhir/client";
+// import {FHIRClient} from "@js-fhir/client";
 
-export let fhirClient = new FHIRClient(process.env['FHIR_BASE_URL'] as string)
+// export let fhirClient = new FHIRClient(process.env['FHIR_BASE_URL'] as string)
