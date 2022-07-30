aab602fcca603b14d70a77161b6e7791ec7afb35
Amolo <railamolo@gmail.com>
2022-07-30 14:56:06 +0000

    Fixed patient search

--- ui/src/pages/PatientList.jsx
+++ ui/src/pages/PatientList.jsx
@@ -1,59 +1,67 @@
-import { Typography, Stack, TextField, Button, Container, useMediaQuery } from '@mui/material'
+import { Stack, TextField, Button, Container, useMediaQuery, Snackbar } from '@mui/material'
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import * as qs from 'query-string';
 import Layout from '../components/Layout';
 import { DataGrid } from '@mui/x-data-grid';
 import { getCookie } from '../lib/cookie';
-import { startVisit } from '../lib/startVisit';
-import { FhirApi } from './../lib/api';
+import { FhirApi } from './../lib/api'
 
 export default function PatientList() {
     let [patients, setPatients] = useState([])
     let navigate = useNavigate()
+    let [open, setOpen] = useState(false)
+    let [message, setMessage] = useState(false)
+    let [loading, setLoading] = useState(false)
+    let [name, setName] = useState('')
 
-    let [selected, setSelected] = useState([])
+    let search = async (name) => {
+        // let builder = `${params}`
+        try {
+            setLoading(true)
+            let data = await FhirApi({ url: `/fhir/Patient?name=${name}`, method: 'GET' })
+            let p = data.data.entry.map((i) => {
+                let r = i.resource
+                console.log(r.name)
+                return {
+                    id: r.id, lastName: r.name[0].family,
+                    age: `${(Math.floor((new Date() - new Date(r.birthDate).getTime()) / 3.15576e+10))} years`
+                }
+            })
+            setPatients(p)
+            setLoading(false)
+            return
+        } catch (error) {
+            setMessage(`Could not find the patient ${name}`)
+            setOpen(true)
+            setLoading(false)
+            setPatients([])
+            setTimeout(() => {
+                setOpen(false)
+            }, 1500)
+            return
+        }
+
+
+    }
 
     let getPatients = async () => {
-
-        let data = await FhirApi({ url: '/fhir/Patient?_limit=50', method: 'GET' })
+        setLoading(true)
+        let data = await FhirApi({ url: '/fhir/Patient?_count=100', method: 'GET' })
+        // console.log(data)
         let p = data.data.entry.map((i) => {
             let r = i.resource
+            console.log(r.name)
             return {
-                id: r.id, lastName: r.name[0].family, firstName: r.name[0].given[0],
+                id: r.id, lastName: r.name[0].family,
                 age: `${(Math.floor((new Date() - new Date(r.birthDate).getTime()) / 3.15576e+10))} years`
             }
         })
         setPatients(p)
-    }
-
-    let startPatientVisit = async () => {
-        if (selected.length === 1) {
-            startVisit(selected[0])
-        }
-
-    }
-
-
-    let viewPatient = async () => {
-        if (selected.length === 1) {
-            navigate(`/patients/${selected[0]}`)
-        }
+        setLoading(false)
         return
-    }
 
-    let deletePatients = async () => {
-        if (selected.length > 0) {
-            for (let i of selected) {
-                let data = await FhirApi({ url: `/fhir/Patient/${i}?_cascade=delete`, method: 'DELETE' })
-                console.log(data)
-            }
-            await getPatients()
-            return
 
-        }
-        await getPatients()
-        return
     }
 
     useEffect(() => {
@@ -71,56 +79,57 @@ export default function PatientList() {
     }, [])
 
 
+    const [selectionModel, setSelectionModel] = useState([]);
 
     const columns = [
-        { field: 'id', headerName: 'ID', width: 150 },
+        { field: 'id', headerName: 'Patient ID', width: 150, editable: true },
         { field: 'lastName', headerName: 'Last Name', width: 250, editable: true },
         { field: 'firstName', headerName: 'First Name', width: 250, editable: true },
-        { field: 'age', headerName: 'Age', width: 200 },
-        // { field: 'role', headerName: 'Date of admission', width: 150 }
+        { field: 'age', headerName: 'Age', width: 150 },
     ];
 
     let isMobile = useMediaQuery('(max-width:600px)');
+
+
     let args = qs.parse(window.location.search);
-    // console.log(args)
 
     return (
         <>
             <Layout>
                 <br />
+                <Snackbar
+                    anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                    open={open}
+                    onClose={""}
+                    message={message}
+                    key={"loginAlert"}
+                />
+                <Stack direction="row" gap={1} sx={{ paddingLeft: isMobile ? "1em" : "2em", paddingRight: isMobile ? "1em" : "2em" }}>
+                    <TextField type={"text"} size="small" sx={{ width: "80%" }} placeholder='Patient Name' onChange={e => { setName(e.target.value) }} />
+                    <Button variant="contained" size='small' sx={{ width: "20%", backgroundColor: "#632165" }} onClick={e => { search(name) }} disableElevation>Search</Button>
+                </Stack>
+                <br />
                 <Container maxWidth="lg">
-                    <br />
-                    <Stack direction="row" gap={1} sx={{ paddingLeft: isMobile ? "1em" : "2em", paddingRight: isMobile ? "1em" : "2em" }}>
-                        <TextField type={"text"} size="small" sx={{ width: "80%" }} placeholder='Patient Name or Patient ID' />
-                        <Button variant="contained" size='small' sx={{ width: "20%", backgroundColor: "#632165" }} disableElevation>Search</Button>
-                    </Stack>
-                    <br />
-                    <Stack direction="row" spacing={2} alignContent="right" >
-                        {(!isMobile) && <Typography sx={{ minWidth: (selected.length > 0) ? '30%' : '70%' }}></Typography>}
-                        {(selected.length > 0) &&
-                            <>
-                                <Button variant="contained" onClick={e => { deletePatients() }} disableElevation sx={{ color: "#632165", backgroundColor: "white" }}> 🗑️Delete Patient{(selected.length > 1) && `s`}</Button>                        </>
-                        }
-                        {(selected.length === 1) &&
-                            <>
-                                <Button variant="contained" onClick={e => { startPatientVisit() }} disableElevation sx={{ backgroundColor: "#632165" }}>Start Visit</Button>
-                                <Button variant="contained" onClick={e => { viewPatient() }} disableElevation sx={{ color: "#632165", backgroundColor: "white" }}>View Patient</Button>
-                            </>
-                        }
-                        <Button variant="contained" disableElevation sx={{ backgroundColor: "#632165" }} onClick={e => { navigate('/patient-registration') }}>Create New Patient</Button>
-                    </Stack>
-                    <br />
                     <DataGrid
-                        loading={(patients && (patients.length > 0)) ? false : (patients.length === 0) ? false : true}
+                        loading={loading}
                         rows={patients ? patients : []}
                         columns={columns}
-                        pageSize={30}
-                        rowsPerPageOptions={[30]}
+                        pageSize={10}
+                        rowsPerPageOptions={[10]}
                         checkboxSelection
                         autoHeight
                         disableSelectionOnClick
-                        onSelectionModelChange={e => { setSelected(e) }}
                         onCellEditStop={e => { console.log(e) }}
+                        onSelectionModelChange={(selection) => {
+                            if (selection.length > 1) {
+                                const selectionSet = new Set(selectionModel);
+                                const result = selection.filter((s) => !selectionSet.has(s));
+
+                                setSelectionModel(result);
+                            } else {
+                                setSelectionModel(selection);
+                            }
+                        }}
                     />
                 </Container>
             </Layout>
@@ -128,7 +137,6 @@ export default function PatientList() {
     )
 
 }
-
 
 
 
