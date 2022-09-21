9ca7b5b1adf1d8cba2e048b4851e52703f054a66
Amolo <railamolo@gmail.com>
2022-09-21 05:47:36 +0000

    udisplay stats on admins dashboards

--- api/src/routes/statistics.ts
+++ api/src/routes/statistics.ts
@@ -2,11 +2,8 @@ import express, { Request, Response } from "express";
 import { requireJWTMiddleware as requireJWT, encodeSession, decodeSession } from "../lib/jwt";
 import db from '../lib/prisma'
 
-const router = express.Router()
-router.use(express.json())
-
-
-
+const router = express.Router();
+router.use(express.json());
 
 // Get Dashboard Stats.
 router.get("/dashboard", [requireJWT], async (req: Request, res: Response) => {
@@ -17,29 +14,34 @@ router.get("/dashboard", [requireJWT], async (req: Request, res: Response) => {
             let role = decodedSession.session.role
             let userId = decodedSession.session.userId
             if (!(role === 'ADMINISTRATOR' || role === "FACILITY_ADMINISTRATOR")) {
-                res.statusCode = 401
+                res.statusCode = 401;
                 res.send({ error: `Insufficient Permissions for ${role}`, status: "error" });
-                return
+                return;
             }
             let user = await db.user.findUnique({
                 where: {
                     id: userId
                 }
-            })
+            });
             let users = await db.user.count({
                 where: {
                     ...(user?.facilityKmhflCode) && { facilityKmhflCode: user.facilityKmhflCode }
                 }
-            })
-            let facilities = await db.facility.count();
+            });
+            let facilities = null;
+            if (role === 'ADMINISTRATOR') {
+                facilities = await db.facility.count();
+            }
             res.statusCode = 200;
-            res.json({ status: "success", data: {"Users":users}})
-            return
+            res.json({ status: "success", data: { users, facilities }});
+            return;
         }
-    } catch (error) {
-        console.error(error)
-        res.statusCode = 400
-        res.json(error)
+    } catch(error) {
+        console.error(error);
+        res.statusCode = 400;
+        res.json(error);
         return
     }
 });
+
+export default router
--- ui/src/pages/Index.jsx
+++ ui/src/pages/Index.jsx
@@ -1,22 +1,72 @@
-import { Typography, Stack, TextField, Grid, Container, useMediaQuery, Card, CardContent } from '@mui/material'
+import { Typography, Stack, TextField, Grid, Container, useMediaQuery, Card, CardContent, CircularProgress } from '@mui/material'
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import * as qs from 'query-string';
 import Layout from '../components/Layout';
 import { getCookie } from '../lib/cookie';
-
-import { FhirApi } from './../lib/api'
+import { apiHost } from './../lib/api';
 
 export default function Index() {
     let [patients, setPatients] = useState()
-    let navigate = useNavigate()
-    let [selected, setSelected] = useState([])
+    let navigate = useNavigate();
+    let [data, setData] = useState({});
+    let [role, setRole] = useState(null);
+    let [facilities, setFacilities] = useState([]);
+
+
+    // fetch users
+    let getFacilities = async () => {
+        let data = (await (await fetch(`${apiHost}/admin/facilities`,
+            { method: "GET", headers: { "Content-Type": "application/json", "Authorization": `Bearer ${getCookie("token")}` } })).json())
+        setFacilities(data.facilities);
+        return;
+    }
+
+    // fetch dashboard stats
+    let getStatistics = async () => {
+        let data = (await (await fetch(`${apiHost}/statistics/dashboard`,
+            { method: "GET", headers: { "Content-Type": "application/json", "Authorization": `Bearer ${getCookie("token")}` } })).json())
+        setData(data.data);
+        return;
+    }
+
+    let getProfile = async () => {
+        let _data = (await (await fetch("/auth/me",
+            {
+                method: "GET",
+                headers: { "Content-Type": "application/json", "Authorization": `Bearer ${getCookie("token")}` }
+            })).json());
+        // console.log(_data);
+        setRole(_data.data.role);
+        if (!(_data.data.role === "ADMINISTRATOR" || _data.data.role === "FACILITY_ADMINISTRATOR")) {
+            navigate('/patients');
+            return;
+        }
+        if (_data.data.role === "ADMINISTRATOR") {
+            getFacilities();
+        }
+        return;
+    }
 
 
 
     let isMobile = useMediaQuery('(max-width:600px)');
 
     let args = qs.parse(window.location.search);
+
+
+    useEffect(() => {
+        if (getCookie("token")) {
+            getProfile();
+            getStatistics();
+            return;
+        } else {
+            navigate('/login');
+            window.localStorage.setItem("next_page", "/");
+            return;
+        }
+    }, [])
+
     // console.log(args)
 
     return (
@@ -24,21 +74,17 @@ export default function Index() {
             <Layout>
                 <br />
                 <Container maxWidth="lg">
-                <Typography variant="h5">Welcome</Typography>
-                    <Grid container spacing={1} padding=".5em" >
-                        <Grid item xs={12} md={12} lg={3} >
-                            <StatCard title="Admins" number="34" bg="#D0ADFC" />
+                    {(role === "ADMINISTRATOR" || role === "FACILITY_ADMINISTRATOR") ? <>
+                        <Typography variant="h5">Welcome </Typography>
+                        <Grid container spacing={1} padding=".5em" >
+                            {Object.keys(data).length > 0 && Object.keys(data).map((entry) => {
+                                console.log(data)
+                                return <Grid item xs={12} md={12} lg={3} >
+                                    <StatCard title={(entry).toUpperCase()} number={data[entry]} bg="#D0ADFC" />
+                                </Grid>
+                            })}
                         </Grid>
-                        <Grid item xs={12} md={12} lg={3}>
-                            <StatCard title="Clinicans" number="34" bg="#D0ADFC" />
-                        </Grid>
-                        <Grid item xs={12} md={12} lg={3}>
-                            <StatCard title="CHW" number="34" bg="#D0ADFC" />
-                        </Grid>
-                        <Grid item xs={12} md={12} lg={3}>
-                            <StatCard title="Total Registered" number="34" bg="#D0ADFC" />
-                        </Grid>
-                    </Grid>
+                    </> : <CircularProgress />}
                 </Container>
             </Layout>
         </>
