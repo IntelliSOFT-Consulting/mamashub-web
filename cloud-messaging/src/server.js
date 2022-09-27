22c4e2b39b5c686dc06a56a43953acf8e0e97138
Patrick Nyatindo <pnyatindo@intellisoftkenya.com>
2022-09-27 09:00:54 +0300

    remove body-parser

--- cloud-messaging/src/server.js
+++ cloud-messaging/src/server.js
@@ -1,13 +1,9 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cron from 'node-cron';
 import { init, sendMsgList } from './services/fcm';
 
 const app = express();
 const Port = process.env.PORT || 8000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // set up the cron job to run every day 6:00 pm
 cron.schedule('0 18 * * *', () => {
