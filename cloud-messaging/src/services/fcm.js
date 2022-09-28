1e19f186119b07f1f496f6be81fe05a354ec00f4
Patrick Nyatindo <pnyatindo@intellisoftkenya.com>
2022-09-28 09:55:53 +0300

    send message to topic

--- cloud-messaging/src/services/fcm.js
+++ cloud-messaging/src/services/fcm.js
@@ -3,35 +3,40 @@ import serviceAccount from '../config/fcmConfig.json';
 import getPushList from './nextVisit';
 
 const sendMsg = async msg => {
-    try {
-        const response = await admin.messaging().sendToDevice(msg.token, msg.data);
-        console.log('Successfully sent message:', response);
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    // send to subscribers
+    const response = await admin.messaging().sendToTopic(msg.topic, msg.data);
+    console.log('Successfully sent message:', response);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const sendMsgList = async () => {
-    try {
-        const recipients = await getPushList();
+  try {
+    const recipients = await getPushList();
 
-        if (!recipients || recipients?.total === 0) return;
+    if (!recipients || recipients?.total === 0) return;
 
-        recipients.entry.forEach(async entry => {
-            const token = entry.resource.subject.reference;
-            const data = entry.resource;
-            await sendMsg({ token, data });
-        });
-    } catch (error) {
-        console.log(error);
-    }
+    recipients.entry.forEach(async entry => {
+      const topic = entry.resource?.patient?.reference?.split('/')[1];
+    //   const data = entry.resource;
+      const msg = {
+        body: 'You have an appointment tomorrow at 10:00 AM',
+        title: 'Next Visit Reminder',
+      };
+      await sendMsg({ topic, data: msg });
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const init = () => {
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        databaseURL: process.env.FCM_DB_URL,
-    });
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FCM_DB_URL,
+  });
 };
 
 export { init, sendMsgList };
