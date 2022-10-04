6a13dd637c0b1a4171cdccdd8107156f3980d225
Patrick Nyatindo <pnyatindo@intellisoftkenya.com>
2022-10-04 17:39:58 +0300

    Validate community referral form

--- ui/src/lib/forms/referral.js
+++ ui/src/lib/forms/referral.js
@@ -1,29 +1,70 @@
+import * as yup from 'yup';
+
 const referralForm = {
   'Referral Officer Details': [
     {
       label: 'Name of Referral Officer',
       name: 'referralOfficerName',
       type: 'text',
+      validate: yup.string().required('Name of Referral Officer is required'),
+      width: {
+        xs: 12,
+        sm: 12,
+        md: 12,
+        lg: 6,
+      },
     },
     {
       label: 'Referral Officer Phone Number',
       name: 'referralOfficerPhoneNumber',
       type: 'text',
+      validate: yup
+        .string()
+        .required('Referral Officer Phone Number is required'),
+      width: {
+        xs: 12,
+        sm: 12,
+        md: 12,
+        lg: 6,
+      },
     },
     {
       label: 'Name receiving CHV',
       name: 'nameReceivingCHV',
       type: 'text',
+      validate: yup.string().required('Name receiving CHV is required'),
+      width: {
+        xs: 12,
+        sm: 12,
+        md: 12,
+        lg: 6,
+      },
     },
     {
       label: 'Receiving CHV Phone Number',
       name: 'receivingCHVPhoneNumber',
       type: 'text',
+      validate: yup.string().required('Receiving CHV Phone Number is required'),
+      width: {
+        xs: 12,
+        sm: 12,
+        md: 12,
+        lg: 6,
+      },
     },
     {
       label: 'Name of Community Health Unit',
       name: 'nameOfCommunityHealthUnit',
       type: 'text',
+      validate: yup
+        .string()
+        .required('Name of Community Health Unit is required'),
+      width: {
+        xs: 12,
+        sm: 12,
+        md: 12,
+        lg: 6,
+      },
     },
   ],
   'Referral Details': [
@@ -31,6 +72,15 @@ const referralForm = {
       label: 'Call made by referring officer',
       name: 'callMadeByReferringOfficer',
       type: 'radio',
+      width: {
+        xs: 12,
+        sm: 12,
+        md: 12,
+        lg: 12,
+      },
+      validate: yup
+        .string()
+        .required('Call made by referring officer is required'),
       options: [
         { value: 'Yes', label: 'Yes' },
         { value: 'No', label: 'No' },
@@ -40,6 +90,13 @@ const referralForm = {
       label: 'Describe the services that CHV should provide for the client',
       name: 'servicesProvided',
       type: 'textarea',
+      width: {
+        xs: 12,
+        sm: 12,
+        md: 12,
+        lg: 12,
+      },
+      validate: yup.string().required('Services provided is required'),
     },
   ],
 };
