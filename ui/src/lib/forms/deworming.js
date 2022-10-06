94492c623e2d73bd3a3b6e602c59e4e51f572983
Patrick Nyatindo <pnyatindo@intellisoftkenya.com>
2022-10-06 08:33:01 +0300

    update deworming

---
+++ ui/src/lib/forms/deworming.js
@@ -0,0 +1,38 @@
+import * as yup from 'yup';
+
+const deworming = {
+  Deworming: [
+    {
+      name: 'dewormingGiven',
+      label:
+        'Was deworming (Mebendazole 500mgs) given once in the 2nd trimester?',
+      type: 'radio',
+      validate: yup.string().required('Deworming given is required'),
+      width: {
+        xs: 12,
+        sm: 12,
+        md: 12,
+        lg: 6,
+      },
+      options: [
+        { label: 'Yes', value: 'Yes' },
+        { label: 'No', value: 'No' },
+      ],
+    },
+    {
+      name: 'dewormingGivenDate',
+      label: 'If yes, date given',
+      type: 'date',
+      validate: yup.date(),
+      width: {
+        xs: 12,
+        sm: 12,
+        md: 12,
+        lg: 6,
+      },
+      relevant: formValues => formValues.dewormingGiven === 'Yes',
+    },
+  ],
+};
+
+export default deworming;
--- ui/src/pages/Deworming.jsx
+++ ui/src/pages/Deworming.jsx
@@ -1,138 +1,166 @@
-import { Container, TextField, Stack, Button, Grid, Snackbar, Typography, Divider, useMediaQuery, Radio, RadioGroup, Alert, FormControlLabel, FormLabel } from '@mui/material'
-import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import Layout from '../components/Layout'
-import { getCookie } from '../lib/cookie'
+import {
+  Container,
+  TextField,
+  Stack,
+  Button,
+  Grid,
+  Snackbar,
+  Typography,
+  Divider,
+  useMediaQuery,
+  Radio,
+  RadioGroup,
+  Alert,
+  FormControlLabel,
+  FormLabel,
+} from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Layout from '../components/Layout';
+import { getCookie } from '../lib/cookie';
 import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-import { Box, FormControl, InputLabel, Select, MenuItem, } from '@mui/material'
+import { Box, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import CurrentPatient from '../components/CurrentPatient'
-
+import CurrentPatient from '../components/CurrentPatient';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import Preview from '../components/Preview';
+import FormFields from '../components/FormFields';
+import dewormingFields from '../lib/forms/deworming';
 
 export default function Deworming() {
+  let navigate = useNavigate();
+  let [open, setOpen] = useState(false);
 
-    let navigate = useNavigate()
-    let [open, setOpen] = useState(false)
+  let [visit, setVisit] = useState();
+  let [deworming, setDeworming] = useState({});
+  let [message, setMessage] = useState(false);
+  let isMobile = useMediaQuery('(max-width:600px)');
 
-    let [visit, setVisit] = useState()
-    let [deworming, setDeworming] = useState({})
-    let [message, setMessage] = useState(false)
-    let isMobile = useMediaQuery('(max-width:600px)');
+  const [value, setValue] = useState('1');
 
-    const [value, setValue] = useState('1');
+  const [inputData, setInputData] = useState({});
+  const [preview, setPreview] = useState(false);
 
-    let saveDeworming = async () => {
+  const fieldValues = Object.values(dewormingFields).flat();
+  const validationFields = fieldValues
+    .filter(item => item.validate)
+    .map(item => ({
+      [item.name]: item.validate,
+    }));
 
+  const validationSchema = yup.object({
+    ...Object.assign({}, ...validationFields),
+  });
 
-        return
+  const initialValues = Object.assign(
+    {},
+    ...fieldValues.map(item => ({ [item.name]: '' }))
+  );
+
+  const formik = useFormik({
+    initialValues: {
+      ...initialValues,
+    },
+    validationSchema: validationSchema,
+    // submit form
+    onSubmit: values => {
+      console.log(values);
+      setPreview(true);
+      setInputData(values);
+    },
+  });
+
+  let saveDeworming = async () => {
+    return;
+  };
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  useEffect(() => {
+    let visit = window.localStorage.getItem('currentPatient');
+    if (!visit) {
+      return;
     }
+    setVisit(JSON.parse(visit));
+    return;
+  }, []);
 
+  return (
+    <>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Container sx={{ border: '1px white dashed' }}>
+          <Snackbar
+            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+            open={open}
+            onClose={''}
+            message={message}
+            key={'loginAlert'}
+          />
+          {visit && <CurrentPatient data={visit} />}
+          {preview ? (
+            <Preview
+              title='Patient Registration Preview'
+              format={dewormingFields}
+              data={{ ...inputData }}
+              close={() => setPreview(false)}
+              submit={saveDeworming}
+            />
+          ) : (
+            <form onSubmit={formik.handleSubmit}>
+              <TabContext value={value}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                  <TabList
+                    value={value}
+                    onChange={handleChange}
+                    variant='scrollable'
+                    scrollButtons='auto'
+                  >
+                    <Tab label='Deworming' value='1' />
+                  </TabList>
+                </Box>
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-    useEffect(() => {
-        let visit = window.localStorage.getItem("currentPatient")
-        if (!visit) { return }
-        setVisit(JSON.parse(visit))
-        return
-    }, [])
+                {/* Preventive Services  */}
 
-
-
-    return (
-        <>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-                
-                    <Container sx={{ border: '1px white dashed' }}>
-                        <Snackbar
-                            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-                            open={open}
-                            onClose={""}
-                            message={message}
-                            key={"loginAlert"}
-                        />
-                        {visit && <CurrentPatient data={visit} />}
-
-                        <TabContext value={value}>
-                            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                                <TabList
-                                    value={value}
-                                    onChange={handleChange}
-                                    variant="scrollable"
-                                    scrollButtons="auto">
-                                    <Tab label="Deworming" value="1" />
-                                </TabList>
-                            </Box>
-
-                            {/* Preventive Services  */}
-
-                            <TabPanel value='1'>
-                                <Typography variant='p' sx={{ fontSize: 'large', fontWeight: 'bold' }}>Deworming</Typography>
-                                <Divider />
-                                <Grid container spacing={1} padding=".5em" >
-                                    <Grid item xs={12} md={12} lg={12}>
-                                        <RadioGroup
-                                            row
-                                            aria-labelledby="demo-row-radio-buttons-group-label"
-                                            name="row-radio-buttons-group"
-                                            onChange={e => { console.log(e) }}
-                                        >
-
-                                            <FormControlLabel value={0} sx={{ width: "40%" }} control={<FormLabel />} label="Was deworming (Mebendazole 500mgs) given once in the 2nd trimester? " />
-                                            <FormControlLabel value={"Yes"} control={<Radio />} label="Yes" />
-                                            <FormControlLabel value={"No"} control={<Radio />} label="No" />
-                                        </RadioGroup>
-
-                                    </Grid>
-                                </Grid>
-                                <Divider />
-                                <p></p>
-                                <Grid container spacing={1} padding=".5em" >
-
-                                    <Grid item xs={12} md={12} lg={4}>
-                                        {!isMobile ? <DesktopDatePicker
-                                            label="If yes, date given"
-                                            inputFormat="yyyy-MM-dd"
-                                            value={deworming.dateGiven || null}
-                                            onChange={e => { console.log(e); setDeworming({ ...deworming, dateGiven: e }) }}
-                                            renderInput={(params) => <TextField {...params} size="small" fullWidth />}
-                                        /> :
-                                            <MobileDatePicker
-                                                label="If yes, date given"
-                                                inputFormat="yyyy-MM-dd"
-                                                value={deworming.dateGiven}
-                                                onChange={e => { console.log(e); setDeworming({ ...deworming, dateGiven: e }) }}
-                                                renderInput={(params) => <TextField {...params} size="small" fullWidth />}
-                                            />}
-                                    </Grid>
-                                </Grid>
-                                <p></p>
-                                <Divider />
-                                <p></p>
-                                <Stack direction="row" spacing={2} alignContent="right" >
-                                    {(!isMobile) && <Typography sx={{ minWidth: '80%' }}></Typography>}
-                                    <Button variant='contained' disableElevation sx={{ backgroundColor: 'gray' }}>Cancel</Button>
-                                    <Button variant="contained" onClick={e => { saveDeworming() }} disableElevation sx={{ backgroundColor: "#632165" }}>Save</Button>
-                                </Stack>
-                                <p></p>
-                            </TabPanel>
-
-                        </TabContext>
-                    </Container>
-                
-            </LocalizationProvider>
-        </>
-    )
-
+                <TabPanel value='1'>
+                  <FormFields formData={dewormingFields} formik={formik} />
+                  <p></p>
+                  <Divider />
+                  <p></p>
+                  <Stack direction='row' spacing={2} alignContent='right'>
+                    {!isMobile && (
+                      <Typography sx={{ minWidth: '80%' }}></Typography>
+                    )}
+                    <Button
+                      variant='contained'
+                      disableElevation
+                      sx={{ backgroundColor: 'gray' }}
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      variant='contained'
+                      type='submit'
+                      disableElevation
+                      sx={{ backgroundColor: '#632165' }}
+                    >
+                      Save
+                    </Button>
+                  </Stack>
+                  <p></p>
+                </TabPanel>
+              </TabContext>
+            </form>
+          )}
+        </Container>
+      </LocalizationProvider>
+    </>
+  );
 }
-
-
-
-
