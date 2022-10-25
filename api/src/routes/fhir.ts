7c2c0bb71cd4398256c00a529d91dbc4f3a325ff
Amolo <railamolo@gmail.com>
2022-10-25 14:42:44 +0000

    fix: define FHIR server in .env

--- api/src/routes/fhir.ts
+++ api/src/routes/fhir.ts
@@ -4,6 +4,8 @@ import { createProxyMiddleware, fixRequestBody } from 'http-proxy-middleware';
 
 const router = express.Router()
 
+const FHIR_BASE_URL = process.env['FHIR_BASE_URL']
+
 router.use(express.json())
 
 router.use(function (err: any, req: Request, res: Response, next: NextFunction) {
@@ -13,7 +15,7 @@ router.use(function (err: any, req: Request, res: Response, next: NextFunction) 
 
 router.use('/', [createProxyMiddleware(
     {
-        target: 'https://devhmis.intellisoftkenya.com', changeOrigin: true,
+        target: FHIR_BASE_URL, changeOrigin: true,
         pathRewrite: {
             '^/fhir/old-path': '/fhir/new-path',
         },
