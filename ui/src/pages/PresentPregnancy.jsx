89375cbb35c3e2c622495a0ab5ca68ce674fde6b
Amolo <railamolo@gmail.com>
2022-12-01 08:35:09 +0300

    fix: forms not rendering due to 401 errors

--- ui/src/pages/PresentPregnancy.jsx
+++ ui/src/pages/PresentPregnancy.jsx
@@ -10,27 +10,27 @@ import {
   useMediaQuery,
   CircularProgress,
   Modal,
-} from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Layout from '../components/Layout';
-import { getCookie } from '../lib/cookie';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
-import { Box } from '@mui/material';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Layout from "../components/Layout";
+import { getCookie } from "../lib/cookie";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import { Box } from "@mui/material";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 
-import CurrentPatient from '../components/CurrentPatient';
-import { apiHost, createEncounter } from '../lib/api';
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-import Preview from '../components/Preview';
-import FormFields from '../components/FormFields';
-import presentPregnancyFields from '../lib/forms/presentPregnancy';
+import CurrentPatient from "../components/CurrentPatient";
+import { apiHost, createEncounter, FhirApi } from "../lib/api";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import Preview from "../components/Preview";
+import FormFields from "../components/FormFields";
+import presentPregnancyFields from "../lib/forms/presentPregnancy";
 
 export default function PresentPregnancy() {
   let [patient, setPatient] = useState({});
@@ -39,7 +39,7 @@ export default function PresentPregnancy() {
   let [loading, setLoading] = useState(false);
   let [message, setMessage] = useState(false);
   let [observations, setObservations] = useState([]);
-  let isMobile = useMediaQuery('(max-width:600px)');
+  let isMobile = useMediaQuery("(max-width:600px)");
   const [newVisit, setNewVisit] = useState(false);
   let [presentPregnancy, setPresentPregnancy] = useState({});
   let [presentPregnancyEncounters, setPresentPregnancyEncounters] = useState(
@@ -47,7 +47,7 @@ export default function PresentPregnancy() {
   );
   const handleClose = () => setOpenModal(false);
   const handleOpen = () => setOpenModal(true);
-  const [value, setValue] = useState('1');
+  const [value, setValue] = useState("1");
   let [openModal, setOpenModal] = useState(false);
 
   const [inputData, setInputData] = useState({});
@@ -55,8 +55,8 @@ export default function PresentPregnancy() {
 
   const fieldValues = Object.values(presentPregnancyFields).flat();
   const validationFields = fieldValues
-    .filter(item => item.validate)
-    .map(item => ({
+    .filter((item) => item.validate)
+    .map((item) => ({
       [item.name]: item.validate,
     }));
 
@@ -66,7 +66,7 @@ export default function PresentPregnancy() {
 
   const initialValues = Object.assign(
     {},
-    ...fieldValues.map(item => ({ [item.name]: '' }))
+    ...fieldValues.map((item) => ({ [item.name]: "" }))
   );
 
   const formik = useFormik({
@@ -75,7 +75,7 @@ export default function PresentPregnancy() {
     },
     validationSchema: validationSchema,
     // submit form
-    onSubmit: values => {
+    onSubmit: (values) => {
       console.log(values);
       setPreview(true);
       setInputData(values);
@@ -94,12 +94,12 @@ export default function PresentPregnancy() {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-  
+
   useEffect(() => {
-    let visit = window.localStorage.getItem('currentPatient');
+    let visit = window.localStorage.getItem("currentPatient");
     if (!visit) {
       setMessage(
-        'No patient visit not been initiated. To start a visit, Select a patient in the Patients list'
+        "No patient visit not been initiated. To start a visit, Select a patient in the Patients list"
       );
       setOpen(true);
       setTimeout(() => {
@@ -112,10 +112,10 @@ export default function PresentPregnancy() {
   }, []);
 
   useEffect(() => {
-    let visit = window.localStorage.getItem('currentPatient');
+    let visit = window.localStorage.getItem("currentPatient");
     if (!visit) {
       prompt(
-        'No patient visit not been initiated. To start a visit, Select a patient in the Patients list'
+        "No patient visit not been initiated. To start a visit, Select a patient in the Patients list"
       );
       return;
     }
@@ -124,14 +124,14 @@ export default function PresentPregnancy() {
   }, []);
 
   useEffect(() => {
-    let visit = window.localStorage.getItem('currentPatient') ?? null;
+    let visit = window.localStorage.getItem("currentPatient") ?? null;
     visit = JSON.parse(visit) ?? null;
     if (visit) {
       getPresentPregnancyEncounters(visit.id);
     }
   }, []);
 
-  let getEncounterObservations = async encounter => {
+  let getEncounterObservations = async (encounter) => {
     setObservations([]);
     // handleOpen();
     let observations = await (
@@ -141,19 +141,19 @@ export default function PresentPregnancy() {
     return observations.observations;
   };
 
-  let getPresentPregnancyEncounters = async patientId => {
+  let getPresentPregnancyEncounters = async (patientId) => {
     setLoading(true);
     let encounters = await (
-      await fetch(
-        `${apiHost}/crud/encounters?patient=${patientId}&encounterCode=${'PRESENT_PREGNANCY'}`
-      )
-    ).json();
+      await FhirApi({
+        url: `/crud/encounters?patient=${patientId}&encounterCode=${"PRESENT_PREGNANCY"}`,
+      })
+    ).data;
     console.log(encounters);
     setPresentPregnancyEncounters(encounters.encounters);
     setLoading(false);
     return;
   };
-  let savePresentPregnancy = async values => {
+  let savePresentPregnancy = async (values) => {
     //get current patient
     if (!visit) {
       prompt(
@@ -164,25 +164,25 @@ export default function PresentPregnancy() {
     let patient = visit.id;
     try {
       //create Encounter
-      let encounter = await createEncounter(patient, 'PRESENT_PREGNANCY');
+      let encounter = await createEncounter(patient, "PRESENT_PREGNANCY");
       // console.log(encounter)
 
       //Create and Post Observations
       let res = await (
         await fetch(`${apiHost}/crud/observations`, {
-          method: 'POST',
+          method: "POST",
           body: JSON.stringify({
             patientId: patient,
             encounterId: encounter,
             observations: values,
           }),
-          headers: { 'Content-Type': 'application/json' },
+          headers: { "Content-Type": "application/json" },
         })
       ).json();
       console.log(res);
 
-      if (res.status === 'success') {
-        prompt('Present Pregnancy saved successfully');
+      if (res.status === "success") {
+        prompt("Present Pregnancy saved successfully");
         // setValue('2')
         await getPresentPregnancyEncounters(patient);
         setNewVisit(false);
@@ -198,36 +198,36 @@ export default function PresentPregnancy() {
     }
   };
 
-  const ordinalSuffix = i => {
+  const ordinalSuffix = (i) => {
     let j = i % 10,
       k = i % 100;
     if (j === 1 && k !== 11) {
-      return i + 'st';
+      return i + "st";
     }
     if (j === 2 && k !== 12) {
-      return i + 'nd';
+      return i + "nd";
     }
     if (j === 3 && k !== 13) {
-      return i + 'rd';
+      return i + "rd";
     }
-    return i + 'th';
+    return i + "th";
   };
 
   return (
     <>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <Container sx={{ border: '1px white dashed' }}>
+        <Container sx={{ border: "1px white dashed" }}>
           <Snackbar
-            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+            anchorOrigin={{ vertical: "top", horizontal: "center" }}
             open={open}
-            onClose={''}
+            onClose={""}
             message={message}
-            key={'loginAlert'}
+            key={"loginAlert"}
           />
           {visit && <CurrentPatient data={visit} />}
           {preview ? (
             <Preview
-              title='Present Pregnancy Preview'
+              title="Present Pregnancy Preview"
               format={presentPregnancyFields}
               data={{ ...inputData }}
               close={() => setPreview(false)}
@@ -236,44 +236,44 @@ export default function PresentPregnancy() {
           ) : (
             <form onSubmit={formik.handleSubmit}>
               <TabContext value={value}>
-                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
                   <TabList
                     value={value}
                     onChange={handleChange}
-                    variant='scrollable'
-                    scrollButtons='auto'
-                    aria-label='scrollable auto tabs example'
+                    variant="scrollable"
+                    scrollButtons="auto"
+                    aria-label="scrollable auto tabs example"
                   >
-                    <Tab label='Present Pregnancy' value='1' />
+                    <Tab label="Present Pregnancy" value="1" />
                   </TabList>
                 </Box>
-                <TabPanel value='1'>
+                <TabPanel value="1">
                   {/* <p></p> */}
                   {!newVisit && (
-                    <Grid container spacing={1} padding='.5em'>
+                    <Grid container spacing={1} padding=".5em">
                       {presentPregnancyEncounters.length > 0 &&
                         presentPregnancyEncounters.map((x, index) => {
                           return (
                             <Grid item xs={12} md={12} lg={12} key={index}>
                               <Button
-                                variant='contained'
-                                onClick={e => {
+                                variant="contained"
+                                onClick={(e) => {
                                   getEncounterObservations(x.resource.id);
                                   handleOpen();
                                 }}
                                 sx={{
-                                  backgroundColor: '#b58dab',
-                                  width: '100%',
-                                  padding: '1rem',
-                                  position: 'relative',
+                                  backgroundColor: "#b58dab",
+                                  width: "100%",
+                                  padding: "1rem",
+                                  position: "relative",
                                 }}
                               >
                                 {`${ordinalSuffix(index + 1)} Contact`}
 
                                 <ArrowForwardIosIcon
                                   sx={{
-                                    position: 'absolute',
-                                    right: '1rem',
+                                    position: "absolute",
+                                    right: "1rem",
                                   }}
                                 />
                               </Button>
@@ -287,18 +287,18 @@ export default function PresentPregnancy() {
                           md={12}
                           lg={12}
                           sx={{
-                            display: 'flex',
-                            justifyContent: 'center',
-                            marginTop: '2rem',
+                            display: "flex",
+                            justifyContent: "center",
+                            marginTop: "2rem",
                           }}
                         >
                           <Button
-                            variant='contained'
-                            onClick={e => {
+                            variant="contained"
+                            onClick={(e) => {
                               setNewVisit(true);
                             }}
                             sx={{
-                              backgroundColor: '#632165',
+                              backgroundColor: "#632165",
                             }}
                           >
                             Add a visit
@@ -325,22 +325,22 @@ export default function PresentPregnancy() {
                       <p></p>
                       <Divider />
                       <p></p>
-                      <Stack direction='row' spacing={2} alignContent='right'>
+                      <Stack direction="row" spacing={2} alignContent="right">
                         {!isMobile && (
-                          <Typography sx={{ minWidth: '80%' }}></Typography>
+                          <Typography sx={{ minWidth: "80%" }}></Typography>
                         )}
                         <Button
-                          variant='contained'
+                          variant="contained"
                           disableElevation
-                          sx={{ backgroundColor: 'gray' }}
+                          sx={{ backgroundColor: "gray" }}
                         >
                           Cancel
                         </Button>
                         <Button
-                          variant='contained'
-                          type='submit'
+                          variant="contained"
+                          type="submit"
                           disableElevation
-                          sx={{ backgroundColor: '#632165' }}
+                          sx={{ backgroundColor: "#632165" }}
                         >
                           Save
                         </Button>
@@ -355,18 +355,18 @@ export default function PresentPregnancy() {
           <Modal
             keepMounted
             open={openModal}
-            sx={{ overflow: 'scroll' }}
+            sx={{ overflow: "scroll" }}
             onClose={handleClose}
           >
             <Box
               sx={{
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                width: '80%',
-                bgcolor: 'background.paper',
-                border: '2px solid #000',
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: "80%",
+                bgcolor: "background.paper",
+                border: "2px solid #000",
                 boxShadow: 24,
                 p: 4,
               }}
@@ -375,7 +375,7 @@ export default function PresentPregnancy() {
               {((observations && observations.length < 1) || !observations) && (
                 <>
                   <CircularProgress />
-                  <Typography variant='h6'>Loading</Typography>
+                  <Typography variant="h6">Loading</Typography>
                 </>
               )}
               <Grid container columnSpacing={1}>
@@ -386,9 +386,9 @@ export default function PresentPregnancy() {
                         <Grid item lg={4} xl={6} md={6} sm={12}>
                           <Box
                             sx={{
-                              padding: '1em',
-                              border: '1px grey solid',
-                              borderRadius: '10px',
+                              padding: "1em",
+                              border: "1px grey solid",
+                              borderRadius: "10px",
                             }}
                           >
                             {/* <Typography sx={{ fontWeight: "bold" }} variant="p">Time: {new Date(observation.resource.meta.lastUpdated).toUTCString()}</Typography><br /> */}
@@ -398,17 +398,17 @@ export default function PresentPregnancy() {
                                 (entry, index) => {
                                   return (
                                     <React.Fragment key={index}>
-                                      <Typography variant='h6' >
+                                      <Typography variant="h6">
                                         {entry.display}
                                       </Typography>
-                                      <Typography variant='p'>
+                                      <Typography variant="p">
                                         {observation.resource.valueQuantity
                                           ? observation.resource.valueQuantity
                                               .value
                                           : observation.resource.valueString ??
                                             observation.resource
                                               .valueDateTime ??
-                                            '-'}
+                                            "-"}
                                       </Typography>
                                       {/* <br /> */}
                                     </React.Fragment>
